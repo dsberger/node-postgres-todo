6c08e9a5db8e005f1ea8b83cb96ddf8a8559fd09
Dan Berger <dsberger@gmail.com>
2016-03-25 11:43:17 -0700

    extracts all CRUD actions to databaseQuery function

--- routes/index.js
+++ routes/index.js
@@ -9,116 +9,75 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
-/* create a todo, POST todos */
+function queryDatabase(res, sqlQuery, sqlArgs){
 
-router.post('/api/v1/todos', function(req, res){
     var results = [];
+
+    pg.connect(connectionString, function(err, client, done) {
+        if(err){
+            done();
+            console.log(err);
+            return res.status(500).json({success: false, data: err});
+        }
+
+        if (sqlQuery){
+            client.query(sqlQuery, sqlArgs);
+        }
+
+        var query = client.query("SELECT * FROM items ORDER BY id ASC");
+
+        query.on('row', function(row){
+            results.push(row);
+        });
+
+        query.on('end', function(){
+            done();
+            return res.json(results);
+        });
+    });
+};
+
+/* CREATE */
+router.post('/api/v1/todos', function(req, res){
 
     var data = {text: req.body.text, complete: false};
 
-    pg.connect(connectionString, function(err, client, done) {
-        if(err){
-            done();
-            console.log(err);
-            return res.status(500).json({success: false, data: err});
-        }
-
-        client.query("INSERT INTO items(text, complete) values($1, $2)", [data.text, data.complete]);
-
-        var query = client.query("SELECT * FROM items ORDER BY id ASC");
-
-        query.on('row', function(row){
-            results.push(row);
-        });
-
-        query.on('end', function(){
-            done();
-            return res.json(results);
-        });
-    });
+    queryDatabase(
+            res,
+            "INSERT INTO items(text, complete) values($1, $2)",
+            [data.text, data.complete]
+        );
 });
 
+/* READ */
 router.get('/api/v1/todos', function(req, res){
-    var results = [];
 
-    pg.connect(connectionString, function(err, client, done) {
-
-        if(err){
-            done();
-            console.log(err);
-            return res.status(500).json({success:false, data: err});
-        }
-
-        var query = client.query("SELECT * FROM items ORDER BY id ASC");
-
-        query.on('row', function(row){
-            results.push(row);
-        });
-
-        query.on('end', function(){
-            done();
-            return res.json(results);
-        });
-    });
+    queryDatabase(res);
 });
 
+/* UPDATE */
 router.put('/api/v1/todos/:todo_id', function(req, res){
-    var results = [];
 
     var id = req.params.todo_id;
-
     var data = {text: req.body.text, complete: req.body.complete};
 
-    pg.connect(connectionString, function(err, client, done){
-
-        if(err){
-            done();
-            console.log(err);
-            return res.status(500).json({success: false, data: err});
-        }
-
-        client.query("UPDATE items SET text=($1), complete=($2) WHERE id=($3)", [data.text, data.complete, id]);
-
-        var query = client.query("SELECT * FROM items ORDER BY id ASC");
-
-        query.on('row', function(row){
-            results.push(row);
-        });
-
-        query.on('end', function(){
-            done();
-            return res.json(results);
-        });
-    });
+    queryDatabase(
+            res,
+            "UPDATE items SET text=($1), complete=($2) WHERE id=($3)",
+            [data.text, data.complete, id]
+        );
 });
 
+/* DELETE */
 router.delete('/api/v1/todos/:todo_id', function(req, res){
-
-    var results = [];
 
     var id = req.params.todo_id;
 
-    pg.connect(connectionString, function(err, client, done){
-
-        if(err){
-            done();
-            console.log(err);
-            return res.status(500).json({success: false, data: err});
-        }
-
-        client.query("DELETE FROM items WHERE id=($1)", [id]);
-
-        var query = client.query("SELECT * FROM items ORDER BY id ASC");
-
-        query.on('row', function(row){
-            results.push(row);
-        });
-
-        query.on('end', function(){
-            done();
-            return res.json(results);
-        });
-    });
+    queryDatabase(
+            res,
+            "DELETE FROM items WHERE id=($1)",
+            [id]
+        );
 });
 
 module.exports = router;
