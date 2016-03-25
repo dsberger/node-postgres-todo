fbb6520a6176f1da0ae5cd24bd2919fdf263247f
Dan Berger <dsberger@gmail.com>
2016-03-25 10:53:06 -0700

    todo CRUD routes complete

--- routes/index.js
+++ routes/index.js
@@ -58,4 +58,63 @@ router.get('/api/v1/todos', function(req, res){
     });
 });
 
+router.put('/api/v1/todos/:todo_id', function(req, res){
+    var results = [];
+
+    var id = req.params.todo_id;
+
+    var data = {text: req.body.text, complete: req.body.complete};
+
+    pg.connect(connectionString, function(err, client, done){
+
+        if(err){
+            done();
+            console.log(err);
+            return res.status(500).json({success: false, data: err});
+        }
+
+        client.query("UPDATE items SET text=($1), complete=($2) WHERE id=($3)", [data.text, data.complete, id]);
+
+        var query = client.query("SELECT * FROM items ORDER BY id ASC");
+
+        query.on('row', function(row){
+            results.push(row);
+        });
+
+        query.on('end', function(){
+            done();
+            return res.json(results);
+        });
+    });
+});
+
+router.delete('/api/v1/todos/:todo_id', function(req, res){
+
+    var results = [];
+
+    var id = req.params.todo_id;
+
+    pg.connect(connectionString, function(err, client, done){
+
+        if(err){
+            done();
+            console.log(err);
+            return res.status(500).json({success: false, data: err});
+        }
+
+        client.query("DELETE FROM items WHERE id=($1)", [id]);
+
+        var query = client.query("SELECT * FROM items ORDER BY id ASC");
+
+        query.on('row', function(row){
+            results.push(row);
+        });
+
+        query.on('end', function(){
+            done();
+            return res.json(results);
+        });
+    });
+});
+
 module.exports = router;
